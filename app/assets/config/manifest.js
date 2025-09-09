e3a9977f76c6ea98174f0c3e20ff3c2d0a324c60
dpurda <purdaioandaniel@gmail.com>
2025-09-09 16:15:47 +0300

    refactor: Update asset manifest to link stylesheets and javascripts from builds directory

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,3 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_directory ../javascripts .js
-//= link_tree ../builds
+//= link_directory ../builds .css
+//= link_directory ../builds .js
